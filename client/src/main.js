ef796022f2d7a6230a27d5201d1d4d70f815e061
Akira <62070215@kmitl.ac.th>
2022-01-24 12:44:16 +0700

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,5 @@ import App from './App.vue'
 import router from './router'
 import './assets/css/style.css'
 import '@fortawesome/fontawesome-free/js/all.js';
-import ThailandAutoComplete from 'vue-thailand-address-autocomplete'
 
-createApp(App).use(router).component('ThailandAutoComplete', ThailandAutoComplete).mount('#app')
+createApp(App).use(router).mount('#app')
