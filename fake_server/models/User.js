d768e11809d23b5f7fa800ead9fac65bad36917b
phalat080 <phalat18@gmail.com>
2022-02-13 14:15:43 +0700

    login register new

--- fake_server/models/User.js
+++ fake_server/models/User.js
@@ -1,43 +1,56 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const UserSchama = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      max: 50,
-      unique: true,
+const UserSchama = new mongoose.Schema({
+    email:{
+        type:String,
+        required: true,
+        max: 50,
+        unique: true,
     },
-    pass: {
-      type: String,
-      required: true,
-      min: 6,
+    pass:{
+        type:String,
+        required: true,
+        min: 6,
     },
-    fname: {
-      type: String,
-      required: true,
-      max: 50,
+    fname:{
+        type:String,
+        required: true,
+        max:50
     },
-    lname: {
-      type: String,
-      required: true,
-      max: 50,
+    lname:{
+        type:String,
+        required: true,
+        max:50
     },
-    idcard: {
-      type: String,
-      required: true,
-      min: 13,
-      max: 13,
+    idcard:{
+        type:String,
+        min: 13,
+        max: 13
     },
-    lineid: {
-      type: String,
-      required: false,
-      max: 50,
+    lineid:{
+        type:String,
+        required: true,
+        max: 50
     },
-  },
-  {
-    timestamps: true,
-  }
+    phone:{
+        type:String,
+        required: true,
+        min: 10,
+        max: 10
+    },
+    booking:{
+        type: Array,
+        local: {
+            type: String,
+        },
+        date: {
+            type:Date
+        }
+    }
+},
+{
+    timestamps: true
+}
 );
 
 module.exports = mongoose.model("User", UserSchama);
