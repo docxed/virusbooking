f2e5aecbdd686867f5b1b4cff9009f082c3b2e99
phalat080 <phalat18@gmail.com>
2022-02-13 17:18:18 +0700

    fix bedsdealing

--- fake_server/routes/bedsdealing.js
+++ fake_server/routes/bedsdealing.js
@@ -1,4 +1,5 @@
 const Bedsdealing = require('../models/Bedsdealing');
+const Beds = require('../models/Beds');
 const router = require("express").Router();
 
 router.post('/bedsdealing', async (req, res) =>{
@@ -8,9 +9,22 @@ router.post('/bedsdealing', async (req, res) =>{
             bed_id: req.body.bed_id,
             user_id: req.body.user_id
         });
-        
-        const New_dealing = await newdealing.save();
-        res.status(201).json({ status: true, message: "จองสำเร็จ" });
+
+        const oldBeds = await Beds.findById(req.body.bed_id);
+
+        if(oldBeds.amount === 0){
+            res.status(201).json({ status: false, message: "ไม่สามารถจองได้ เนื่องจากเตียงเต็ม" });
+        }else{
+            const update_Beds = await Beds.findByIdAndUpdate(req.body.bed_id,
+                {
+                    $set: {amount: oldBeds.amount - 1},
+                },
+                    { new: true }
+                );
+    
+            const New_dealing = await newdealing.save();
+            res.status(201).json({ status: true, message: "จองสำเร็จ" });
+        }
     } catch (err) {
         res
       .status(500)
