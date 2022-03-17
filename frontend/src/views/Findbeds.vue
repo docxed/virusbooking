0f5888163bf8b5296d0bb0f62c9e99296b6d10ff
Akira <54892411+docxed@users.noreply.github.com>
2022-03-17 21:19:52 +0700

    Revert "Update Findbeds.vue"
    This reverts commit b0b4eaf4495e9d4686b783f5306b87cb25b9ad86.

--- frontend/src/views/Findbeds.vue
+++ frontend/src/views/Findbeds.vue
@@ -13,6 +13,8 @@ export default {
   },
   methods: {},
   created() {},
+<<<<<<< Updated upstream
+=======
 }
 </script>
 <style scoped>
@@ -31,5 +33,6 @@ export default {
   background-color: white;
   color: black;
   border: 1px solid DodgerBlue;
+>>>>>>> Stashed changes
 }
 </script>
