d9e0ceb86e8e46f8b39fa52d81d1c95fc5386f6d
Akira <54892411+docxed@users.noreply.github.com>
2022-03-17 21:17:35 +0700

    Update Findbeds.vue

--- frontend/src/views/Findbeds.vue
+++ frontend/src/views/Findbeds.vue
@@ -13,26 +13,5 @@ export default {
   },
   methods: {},
   created() {},
-<<<<<<< Updated upstream
-=======
-}
-</script>
-<style scoped>
-.button1:hover {
-  background-color: white;
-  color: black;
-  border: 1px solid #24884f;
-}
-
-.button1 {
-  background-color: #24884f;
-  color: white;
-}
-
-.button2:hover {
-  background-color: white;
-  color: black;
-  border: 1px solid DodgerBlue;
->>>>>>> Stashed changes
 }
 </script>
